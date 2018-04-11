1de240ad170a7c2316cafa6b98f7e15fa6f59b1c
Everett <everett.bowline@gmail.com>
2018-04-11 12:47:04 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,8 @@
 def second_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
+      :patriarch,
+      :matriarch,
       :hero => {
         #your key/value pairs here
         },
