f00d431b965971468eeeec1eb8d43f5ee45ced77
Everett <everett.bowline@gmail.com>
2018-04-11 12:53:26 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,10 +5,10 @@ def third_challenge
         :name => "Lord Montague", :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague", :Age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo", :Age => "15", :Status => "alive"
         },
       :hero_friends => []
    }, 
